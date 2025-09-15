e0eaaad1e7c8a8153681cff27e8d727c8ebb7788
Mijael Segura <seguramijael03@gmail.com>
2025-09-14 21:07:13 -0300

    fixes

--- backend/stylizedai/settings.py
+++ backend/stylizedai/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from dotenv import load_dotenv
 import os
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
